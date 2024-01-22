0aa4e7ded836c2cdb41ec5c9295ff4a85b6e0316
السيد <106372829+jepthoniq@users.noreply.github.com>
2024-01-22 19:07:57 +0300

    Update ⁨الخطوط.py⁩

--- JoKeRUB/plugins/الخطوط.py
+++ JoKeRUB/plugins/الخطوط.py
@@ -46,12 +46,26 @@ async def btext(event):
         await edit_delete(event, "**᯽︙ تم اطفاء خط الرمز بنجاح ✓ **")
         return
 
+@l313l.on(admin_cmd(pattern="(خط الجوكر|خط جوكر)"))
+async def Aljoker(event):
+    Aljoker = gvarstatus("ramz")
+    if not Aljoker:
+        addgvar ("joker", "on")
+        await edit_delete(event, "**᯽︙ تم تفعيل خط الجوكر بنجاح ✓**")
+        return
+
+    if Aljoker:
+        delgvar("joker")
+        await edit_delete(event, "**᯽︙ تم اطفاء خط الجوكر بنجاح ✓ **")
+        return
+
 @l313l.on(events.NewMessage(outgoing=True))
 async def reda(event):
     if event.message.text and not event.message.media and "." not in event.message.text:
         isbold = gvarstatus("bold")
         isramz = gvarstatus("ramz")
         istshwesh = gvarstatus("tshwesh")
+        Aljoker = gvarstatus("joker")
         if isbold:
             try:
                 await event.edit(f"**{event.message.text}**")
@@ -65,5 +79,10 @@ async def reda(event):
         if istshwesh:
             try:
                 await event.edit(f"~~{event.message.text}~~")
+            except MessageIdInvalidError:
+                pass
+        if Aljoker:
+            try:
+                await event.edit(f"```{event.message.text}```")
             except MessageIdInvalidError:
                 pass
