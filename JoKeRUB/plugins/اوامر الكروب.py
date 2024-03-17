4c48120cea3439d735ee74578c06799728248a71
السيد <106372829+jepthoniq@users.noreply.github.com>
2024-03-17 03:18:01 +0300

    Update اوامر الكروب.py

--- JoKeRUB/plugins/اوامر الكروب.py
+++ JoKeRUB/plugins/اوامر الكروب.py
@@ -974,9 +974,10 @@ async def handle_guess(event):
 @l313l.on(events.NewMessage(incoming=True))
 async def handle_incoming_message(event):
     global joker_player, is_game_started2
-    if is_game_started2 and event.raw_text.lower() == "انا" and not joker_player and event.chat_id:
-        joker_player = event.sender_id
-        await event.reply("تم تسجيل مشاركتك في لعبة المحيبس توكل على الله.")
+    if is_game_started2 and event.raw_text.lower() == "انا" and not joker_player:
+        if event.chat_id:
+            joker_player = event.sender_id
+            await event.reply("تم تسجيل مشاركتك في لعبة المحيبس توكل على الله.")
 
 def format_board(game_board, numbers_board):
     formatted_board = ""
