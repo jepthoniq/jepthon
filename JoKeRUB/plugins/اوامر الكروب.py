db01918be8deb9f825b64b0ef81ab61f1bf905f2
السيد <106372829+jepthoniq@users.noreply.github.com>
2024-03-18 20:12:06 +0300

    Update ⁨اوامر الكروب.py⁩

--- JoKeRUB/plugins/اوامر الكروب.py
+++ JoKeRUB/plugins/اوامر الكروب.py
@@ -987,7 +987,7 @@ async def handle_guess(event):
             group_game_status[chat_id]['is_game_started2'] = False
             group_game_status[chat_id]['joker_player'] = None
 
-@l313l.on(events.NewMessage(incoming=True))
+@l313l.on(events.NewMessage(pattern=r'\انا'))
 async def handle_incoming_message(event):
     global group_game_status
     chat_id = event.chat_id
@@ -996,7 +996,7 @@ async def handle_incoming_message(event):
             'is_game_started2': False,
             'joker_player': None
         }
-    if group_game_status[chat_id]['is_game_started2'] and event.raw_text.lower() == "انا" and not group_game_status[chat_id]['joker_player']:
+    if group_game_status[chat_id]['is_game_started2'] and not group_game_status[chat_id]['joker_player']:
         group_game_status[chat_id]['joker_player'] = event.sender_id
         await event.reply(f"**تم تسجيلك في المسابقة روح لحسين بظهرك\n{format_board(game_board, numbers_board)}**")
 
